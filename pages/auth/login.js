70a61dd094da3d672649e0f7ee53aea8f9f1b30d
easylog <easyloghilfe@gmail.com>
2025-04-22 18:46:46 +0200

    Update login.js

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,1 +1,8 @@
-// Login Page UI hier
+export default function Login() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-4">🔐 Login</h1>
+      <p>Login-Formular kommt hier rein...</p>
+    </main>
+  );
+}
