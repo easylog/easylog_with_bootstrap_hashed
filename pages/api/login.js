cbf1d543d1ae0f4e59b52dc5b52a9a2dff164d3c
easylog <easyloghilfe@gmail.com>
2025-04-22 19:27:26 +0200

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -27,6 +27,6 @@ export default async function handler(req, res) {
     return res.status(200).json({ token });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ message: 'Interner Fehler' });
+    return res.status(500).json({ message: 'Interner Serverfehler' });
   }
 }
